7738f7f5bd3f525e562507d855f15160c28a4f75
GermanBluefox <dogafox@gmail.com>
2015-09-25 23:15:45 +0200

    (bluefox) translate to russian

--- www/js/language.js
+++ www/js/language.js
@@ -1,17 +1,20 @@
 var translation = {
-    "Devices":                          {"de": "Geräte"},
-    "The above copyright notice must be enclosed in all copies or substantial portions of the Software.": {"de": "Der obige Urheberrechtsvermerk ist in allen Kopien oder Teilkopien der Software beizulegen."},
-    "Set Name":                         {"de": "Namen ändern"},
-    "Set Location":                     {"de": "Ort ändern"},
-    "Change Parameter(s)":              {"de": "Werte anpassen"},
-    "Value:":                           {"de": "Wert:"},
-    "About OpenZwave Configurator":     {"de": "Über OpenZwave Konfigurator"},
-    "Location:":                        {"de": "Ort:"},
-    "Name:":                            {"de": "Name:"},
-    "Min:":                              {"de": "Min:"},
-    "Max:":                              {"de": "Max:"},
-    "Save":                             {"de": "Speichern"},
-    "Cancel":                           {"de": "Abbrechen"}
+    "Devices":                          {"de": "Geräte",                "ru": "Устройства"},
+    "The above copyright notice must be enclosed in all copies or substantial portions of the Software.": {
+        "de": "Der obige Urheberrechtsvermerk ist in allen Kopien oder Teilkopien der Software beizulegen.",
+        "ru": "Указанное выше уведомление об авторском праве и данные условия должны быть включены во все копии или значимые части данного Программного Обеспечения."
+    },
+    "Set Name":                         {"de": "Namen ändern",          "ru": "Сменить имя"},
+    "Set Location":                     {"de": "Ort ändern",            "ru": "Сменить место"},
+    "Change Parameter(s)":              {"de": "Werte anpassen",        "ru": "Изменить настройки"},
+    "Value:":                           {"de": "Wert:",                 "ru": "Значение"},
+    "About OpenZwave Configurator":     {"de": "Über OpenZwave Konfigurator", "ru": "О конфигураторе"},
+    "Location:":                        {"de": "Ort:",                  "ru": "Место:"},
+    "Name:":                            {"de": "Name:",                 "ru": "Имя:"},
+    "Min:":                             {"de": "Min:",                  "ru": "Минимум:"},
+    "Max:":                             {"de": "Max:",                  "ru": "Максимум:"},
+    "Save":                             {"de": "Speichern",             "ru": "Сохранить"},
+    "Cancel":                           {"de": "Abbrechen",             "ru": "Отмена"}
 };
 
 var missesTranslation = {};
