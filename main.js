2b8473b0823143aa8d879561b377ee0234290bd4
Carsten Burghardt <carsten@cburghardt.com>
2019-08-23 12:35:23 +0200

    Cleanup obsolete states (#63)
    * Cleanup obsolete states
    
    * Cleanup values moved to separate function and just called from value added
    
    * Proposal for confirm dialog
    
    * Revert
    
    * return

--- main.js
+++ main.js
@@ -826,6 +826,26 @@ function extendNode(nodeID, nodeInfo, callback) {
     if (!count && callback) callback();
 }
 
+/*
+* Verify that we don't have duplicate valueIds (same node, instance and index but different label)
+* Can happen when the ozwave library changes labels for devices between releases
+*/
+function cleanupValueId(nodeID, comClass, valueId) {
+    const channelID = calcName(nodeID, comClass);
+    
+    for (var i in objects) {
+        if (!objects.hasOwnProperty(i)) continue;
+        if (i.startsWith(channelID + '.')) {
+            if (objects[i].native && objects[i].native.instance === valueId.instance && objects[i].native.index === valueId.index &&
+            objects[i].common.name !== valueId.label) {
+                adapter.log.info('remove obsolete state:' + objects[i]._id);
+                delObjects([objects[i]]);
+                return;
+            }
+        }
+    }
+}
+
 function extendChannel(nodeID, comClass, valueId) {
     if (!valueId || !comClass) return;
 
@@ -1275,6 +1295,7 @@ function main() {
         extendInclusion();
         adapter.log.debug('value added: nodeID: ' + nodeID + ' comClass: ' + JSON.stringify(comClass) + ' value: '  + JSON.stringify(valueId));
         extendChannel(nodeID, comClass, valueId);
+        cleanupValueId(nodeID, comClass, valueId);
     });
 
     zwave.on('value changed', function (nodeID, comClass, valueId) {
